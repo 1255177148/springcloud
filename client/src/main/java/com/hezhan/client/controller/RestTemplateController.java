8f2a7563abb747c86def5898943729580f355969
elvis.he <elvis.he@gaiaworks.cn>
2019-10-21 14:32:21 +0800

    配合单元测试项目

--- client/src/main/java/com/hezhan/client/controller/RestTemplateController.java
+++ client/src/main/java/com/hezhan/client/controller/RestTemplateController.java
@@ -1,9 +1,9 @@
 package com.hezhan.client.controller;
 
+import com.hezhan.client.entity.User;
 import com.hezhan.client.exception.RemoteException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author hezhan
@@ -16,5 +16,53 @@ public class RestTemplateController {
     @GetMapping("/test")
     public String test(){
         throw new RemoteException("报错了呦");
+    }
+
+    @GetMapping("/testHeader")
+    public String testHeader(@RequestHeader("name") String name){
+        return name;
+    }
+
+    @PutMapping("/testPut")
+    public void testPut(@RequestHeader("name") String name, @RequestBody User user){
+        if (StringUtils.isNotEmpty(name) || "hezhan".equals(name)){
+            if (user == null){
+                throw new RemoteException("body参数为空");
+            } else {
+                if (!"hezhan".equals(user.getName())){
+                    throw new RemoteException("body参数中name字段取值不正确");
+                }
+            }
+        } else {
+            throw new RemoteException("header参数中name字段为空");
+        }
+    }
+
+    @PostMapping("/testPost")
+    public String testPost(@RequestHeader("name") String name, @RequestBody User user){
+        if (StringUtils.isNotEmpty(name) || "hezhan".equals(name)){
+            if (user == null){
+                throw new RemoteException("body参数为空");
+            } else {
+                if (!"hezhan".equals(user.getName())){
+                    throw new RemoteException("body参数中name字段取值不正确");
+                } else {
+                    return "成功";
+                }
+            }
+        } else {
+            throw new RemoteException("header参数中name字段为空");
+        }
+    }
+
+    @DeleteMapping("/testDelete/{id}")
+    public void testDelete(@PathVariable("id") String id, @RequestHeader("name") String name){
+        if (StringUtils.isEmpty(name)){
+            throw new RemoteException("header参数中name字段为空");
+        } else {
+            if (!"hezhan".equals(id)){
+                throw new RemoteException("路径参数不正确");
+            }
+        }
     }
 }
