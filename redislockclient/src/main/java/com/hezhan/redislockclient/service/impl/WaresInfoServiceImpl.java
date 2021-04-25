340ca29992b7fb17ee33258d02f7c5c26923adfc
elvis.he <elvis.he@yunlsp.com>
2021-04-25 18:09:49 +0800

    redis 分布式锁，并发在1000时，由于feign读取超时，熔断了，导致client没有执行完，feign如何使用OKHttp,正在研究中

--- redislockclient/src/main/java/com/hezhan/redislockclient/service/impl/WaresInfoServiceImpl.java
+++ redislockclient/src/main/java/com/hezhan/redislockclient/service/impl/WaresInfoServiceImpl.java
@@ -48,7 +48,7 @@ public class WaresInfoServiceImpl extends ServiceImpl<WaresInfoMapper, WaresInfo
         RLock lock = redissonClient.getLock("lock");
         try {
             // 加分布式锁
-            lock.lock(2L, TimeUnit.SECONDS);
+            lock.lock();
             result = reduceInventory();
 
         } catch (Exception e){
